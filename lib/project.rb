392a0adea942973ef50f9a3724ea192d9987bab6
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 15:00:12 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,23 @@
 class Project
+
+  attr_accessor :title
   
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def backers
+    @backers.dup.freeze
+  end
+  
+  def add_backer(backer)
+    if @backers.include?!(self)
+      project.backer = self
+    end
+    @backed_projects << project
+  end
+  
+end
 end
