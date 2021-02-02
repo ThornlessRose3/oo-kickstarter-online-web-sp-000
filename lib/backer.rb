58e5a6b12790e5d1c9c31d4adf6bdc342a68ca76
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 15:11:03 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,16 @@
 class Backer
   
   attr_accessor :name
-  
+  attr_reader :backed_projects
   
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   
-  def backed_projects
-    @backed_projects.dup.freeze
-  end
+  #def backed_projects
+  #  @backed_projects.dup.freeze
+  #end
   
   def back_project(project)
     if !(project.backers.include?(self))
